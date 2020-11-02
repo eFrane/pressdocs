d7a81dc61d13189b743afaa15749fdd891327c73
eFrane <efrane@meanderingsoul.com>
2020-11-02 15:46:58 +0100

    Rewrite language provider loader

--- packages/vuepress-plugin-pressdocs/src/index.js
+++ packages/vuepress-plugin-pressdocs/src/index.js
@@ -1,17 +1,36 @@
 'use strict'
 
 const isInstalled = require('is-installed')
+const { getLanguages } = require('@pressdocs/lang/lib/Settings')
 const availableLanguages = require('./languages.json')
 
 const plugin = (options, context) => {
     let languageProviders = []
 
-    // initialize available language providers
-    for (const providerPackage of Object.values(availableLanguages)) {
-        if (isInstalled(providerPackage)) {
-            const Provider = require(providerPackage)
-            languageProviders.push(new Provider(options, context))
+    // initialize language providers
+    const configuredLanguages = Object.keys(getLanguages(options))
+
+    for (const [languageIdentifier, providerPackage] of Object.entries(availableLanguages)) {
+        // don't initialize non-configured providers
+        if (!configuredLanguages.includes(languageIdentifier)) {
+            continue
         }
+
+        // they should also be installed
+        if (!isInstalled(providerPackage)) {
+            console.warn(`Language provider ${providerPackage} is configured but not installed`)
+            continue
+        }
+
+        const Provider = require(providerPackage)
+        const providerInstance = new Provider(options, context)
+
+        if (!providerInstance.canRun()) {
+            console.warn(`Language provider ${providerPackage} is missing external dependencies, check \`vuepress pressdocs-lint\` for details`)
+            continue
+        }
+
+        languageProviders.push()
     }
 
     return {
@@ -27,6 +46,13 @@ const plugin = (options, context) => {
                 })
 
             return pages
+        },
+
+        extendCli (cli) {
+            cli.command('pressdocs-lint [targetDir]', 'Information about the pressdocs configuration')
+                .action(() => {
+                    // TODO: implement lint command
+                })
         }
     }
 }
